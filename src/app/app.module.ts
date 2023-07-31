293d955f9e55d60184f2ed375ddd628e9a6cff3f
Nabujjal Nath <nabujjal@gmail.com>
2023-07-31 18:00:19 +0530

    setting routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { SellerAuthComponent } from './seller-auth/seller-auth.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    SellerAuthComponent
   ],
   imports: [
     BrowserModule,
